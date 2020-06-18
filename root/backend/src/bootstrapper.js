7a95862bc86b820259cbe171a3e7f3b7aa6ffc7c
Tobias Marty <hello@tobiasmart.ch>
2020-06-18 21:51:51 +0200

    trying to send example data to frontend

--- root/backend/src/bootstrapper.js
+++ root/backend/src/bootstrapper.js
@@ -5,6 +5,19 @@ import {router} from './routes/router.js';
 class App {
     constructor() {
         this.app = express();
+        this.initCORS();
+    }
+
+    initCORS() {
+        /* init CORS (middleware) */
+        const allowCrossDomain = function(req, res, next) {
+            res.header('Access-Control-Allow-Origin', '*');
+            res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+            res.header('Access-Control-Allow-Headers', 'Content-Type');
+            next();
+        };
+        
+        this.app.use(allowCrossDomain);
     }
 
     loadRoutes() {
--- root/backend/src/controller/indexController.js
+++ root/backend/src/controller/indexController.js
@@ -8,7 +8,11 @@ export class IndexController {
     }
 
     list(req, res) { 
-        res.json({message: "List Test"});
+
+        // test data
+        let listItems = [{"id":7,"name":"New und so","done":false,"createDate":"2020-06-15","finishDate":null,"dueDate":"2020-06-05","prio":1},{"id":1,"name":"Test","done":true,"createDate":"2020-06-14","finishDate":"2020-06-14","dueDate":"2020-06-26","prio":1},{"id":8,"name":"test","done":false,"createDate":"2020-06-15","finishDate":null,"dueDate":"2020-06-02","prio":1},{"id":9,"name":"test","done":false,"createDate":"2020-06-15","finishDate":null,"dueDate":null,"prio":1},{"id":5,"name":"zkzku","done":false,"createDate":"2020-06-14","finishDate":null,"dueDate":null,"prio":1},{"id":2,"name":"aasdfasf","done":false,"createDate":"2020-05-14","finishDate":null,"dueDate":"2020-06-17","prio":"2"},{"id":3,"name":"afewee","done":true,"createDate":"2020-06-04","finishDate":"2020-06-14","dueDate":null,"prio":"2"},{"id":4,"name":"awefewf","done":false,"createDate":"2020-06-14","finishDate":null,"dueDate":"2020-06-17","prio":"2"},{"id":6,"name":"Testaaa","done":false,"createDate":"2020-06-14","finishDate":null,"dueDate":null,"prio":"3"}];
+        
+        res.json(listItems);
         res.end();
     }
 }
